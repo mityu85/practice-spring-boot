374e7a0e13efb3073d618ec27d059064cd3bb114
Mihaly Toth <Mihaly.Toth@kuka.com>
2021-09-06 16:28:18 +0200

    first commit

---
+++ kepesitovizsga-gyak/src/main/java/training360/KepesitovizsgaGyakApplication.java
@@ -0,0 +1,13 @@
+package training360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KepesitovizsgaGyakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KepesitovizsgaGyakApplication.class, args);
+	}
+
+}
